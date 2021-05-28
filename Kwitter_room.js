a532bf6278d43ab1609c86672273c744566cbb42
NaethanLeo <75014295+NaethanLeo@users.noreply.github.com>
2021-05-28 14:05:27 +0530

    Add files via upload

---
+++ Kwitter_room.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAyXc0bjueXBG69slyWnWm0_EAEMoxkueo",
+    authDomain: "project-93---95-chat-app.firebaseapp.com",
+    databaseURL: "https://project-93---95-chat-app-default-rtdb.firebaseio.com",
+    projectId: "project-93---95-chat-app",
+    storageBucket: "project-93---95-chat-app.appspot.com",
+    messagingSenderId: "192731928086",
+    appId: "1:192731928086:web:af9d1422553f4d51174737"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
